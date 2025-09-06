c8b1e0b09ba529ad13f39fac3ae37399e5f4f78b
fielnyrb <brynleif@brynleif.com>
2025-09-06 14:14:11 +0100

    Fire and water, earth and air

---
+++ Firefight/Assets/Scenes/Experiments/Fire/WaterController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move to the right constantly
+        transform.Translate(Vector2.right * Time.deltaTime * 2);
+    }
+}
